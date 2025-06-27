1c9f1e2a148946c16d7a883fd454e2dcee1e14f1
Slowlor1ss <85391319+Slowlor1ss@users.noreply.github.com>
2025-06-27 17:03:08 +0100

    Updated version number

--- SlackPaint.py
+++ SlackPaint.py
@@ -14,7 +14,7 @@ import json
 from io import BytesIO
 import urllib.request
 
-__version__ = "v0.2.0-beta"
+__version__ = "v0.2.1-beta"
 
 def check_for_update():
     updater = Updater(__version__)
